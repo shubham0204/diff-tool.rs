7e0c845fa2fa63bb4ae51e664c0018d4732f0c65
Shubham Panchal <panchalshubhamdev@gmail.com>
2024-03-13 19:05:45 +0530

    [new] added compute_lcs_sequences() function in src/lcs.rs

--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 
 ## Development Log
 
-* 13-03-2024: Added `compute_lcs` in `src/lcs.rs` ( [Reference](https://www.programiz.com/dsa/longest-common-subsequence) )
+* 13-03-2024: Added `compute_lcs` in `src/lcs.rs` and test-cases ( [Reference](https://www.programiz.com/dsa/longest-common-subsequence) )
 * 12-03-2024: Project initiated from [codingchallenges.fyi](https://codingchallenges.fyi/challenges/challenge-diff)
 
 ## Useful Resources
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,22 @@
 mod lcs;
 use lcs::compute_lcs;
+use lcs::compute_lcs_sentences;
+use std::env;
+use std::fs;
+
+fn read_file( filepath: &String ) -> String {
+    fs::read_to_string( filepath ).expect( "Could not read to string" )
+}
 
 fn main() {
-    let seq1: &str = "ABCD" ; 
-    let seq2: &str = "PQRSABXY" ; 
-    let lcs = compute_lcs(seq1, seq2) ; 
-    println!( "{}" , lcs ) ;
+    let args: Vec<String> = env::args().collect() ; 
+    let contents1: String = read_file( &args[1] ) ;
+    let contents2: String = read_file( &args[2] ) ; 
+    let lines1: Vec<&str> = contents1.split( "\n" ).collect() ;
+    let lines2: Vec<&str> = contents2.split( "\n" ).collect() ;
+
+    let lcs = compute_lcs_sentences(lines1, lines2) ;
+    for line in lcs {
+        println!( "{}" , line ) ; 
+    }
 }
